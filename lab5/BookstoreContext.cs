ad3ef57e96a4ace29e1d38e47bd8f03a0ae549b9
Chrrypgy <cherry.pogoy.5@gmail.com>
2025-02-24 12:17:01 +0800

    lab5

---
+++ lab5/BookstoreContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace lab5
+{
+    public class BookstoreContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; } = new HashSet<Book>();
+        public DbSet<Author> Authors { get; set; } = new HashSet<Author>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)  // Fixed typo
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Userdb;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        }
+    }
+
+    public class Author
+    {
+        public int AuthorID { get; set; }
+        public required string Name { get; set; }
+        public List<Book> Books { get; set; } = new();
+    }
+
+    public class Book
+    {
+        public int BookID { get; set; }
+        public required string Title { get; set; }
+        public int AuthorID { get; set; }
+        public required Author Author { get; set; }
+    }
+}
